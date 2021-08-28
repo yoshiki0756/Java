f025fa1d0ea5d8ae8589ce1a4bafe2499b02e0ab
yoshiki0756 <g18lb014.yoshiki@gmail.com>
2021-08-28 21:17:48 +0900

    commit -m"[add]java_practice"

---
+++ Java/pleiades/workspace/JavaWcp/src/Chapter04.java
@@ -0,0 +1,12 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int LUKKY_NUMBER = 7;
+		String str = "中村";
+		System.out.println(str);
+		str = "まずはやってみる";
+		System.out.println(str);
+		System.out.println(LUKKY_NUMBER);
+	}
+
+}
