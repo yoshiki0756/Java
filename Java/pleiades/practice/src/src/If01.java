9293acdadff2a60fd4ff3cf5bd0209c56d9a0516
yoshiki0756 <g18lb014.yoshiki@gmail.com>
2021-08-31 00:32:17 +0900

    first commit

--- Java/pleiades/practice/src/src/If01.java
+++ Java/pleiades/practice/src/src/If01.java
@@ -7,11 +7,9 @@ public class If01 {
 		if (number < 5) {
 			System.out.println(number + "は5未満です");
 		}
-		
 		if (number <10) {
 			System.out.println(number + "は10未満です");
 		}
-		
 		if (number < 15) {
 			System.out.println(number + "は15未満です");
 		}
