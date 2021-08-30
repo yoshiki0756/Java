9f9480bb475d4e132aec05191e7779a1398a1b97
yoshiki0756 <g18lb014.yoshiki@gmail.com>
2021-08-30 23:41:10 +0900

    commit -m"[add]if"

---
+++ Java/pleiades/practice/src/src/If01.java
@@ -0,0 +1,17 @@
+package src;
+
+public class If01 {
+	public static void main(String[] ages) {
+		int number = 7;
+		if (number < 5) {
+			System.out.println(number + "は5未満です");
+		}
+		if (number <10) {
+			System.out.println(number + "は10未満です");
+		}
+		if (number < 15) {
+			System.out.println(number + "は15未満です");
+		}
+	}
+
+}
