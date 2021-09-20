9f95b00a8c94b73350f6feaa6b46b894dee260a5
yoshiki0756 <g18lb014.yoshiki@gmail.com>
2021-09-20 23:33:33 +0900

    commit -m"[Stsart]chapter10"

--- Java/pleiades/practice/src/java_Book/chapter10/Hero.java
+++ Java/pleiades/practice/src/java_Book/chapter10/Hero.java
@@ -1,5 +1,19 @@
 package java_Book.chapter10;
 
+import java_Book.chapter8.Matango;
+
 public class Hero {
+	String name = "ミナト";
+	int hp = 100;
+	
+	public void attack(Matango m) {
+		System.out.println(this.name + "の攻撃！");
+		m.hp -= 5;
+		System.out.println("5ポイントのダメージをあたえた！");
+	}
+	
+	public void run() {
+		System.out.println(this.name + "は逃げ出した！");
+	}
 
 }
--- Java/pleiades/practice/src/java_Book/chapter10/SuperHero.java
+++ Java/pleiades/practice/src/java_Book/chapter10/SuperHero.java
@@ -1,5 +1,21 @@
 package java_Book.chapter10;
 
+import java_Book.chapter8.Matango;
+
 public class SuperHero {
+	String name = "ミナト";
+	int hp = 100;
+	boolean flying;
+	
+	public void attack(Matango m) {
+		System.out.println(this.name + "の攻撃！");
+		m.hp -= 5;
+		System.out.println("5ポイントのダメージを与えた！");
+	}
+	
+	public void run() {
+		System.out.println(this.name + "は逃げ出した！");
+	}
+	
 
 }
